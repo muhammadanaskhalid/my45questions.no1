87c7e4cbd031bbb10d8dbff2ee9ac04a555962fd
muhammadanaskhalid <168372939+muhammadanaskhalid@users.noreply.github.com>
2024-05-03 01:22:41 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let PersonName = "Eric";
+console.log(` Hello ${PersonName} would you like to learn some Python today?`);
